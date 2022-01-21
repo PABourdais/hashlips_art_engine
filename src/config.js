5b1bd9e2fdb4e69c2e033139f21003b367ae28e5
PABourdais <pierre.alex.bourdais@gmail.com>
2022-01-21 12:34:21 +0100

    add some new attributs

--- src/config.js
+++ src/config.js
@@ -95,7 +95,7 @@ const layerConfigurations = [
     growEditionSizeTo: 500,
     layersOrder: [
       { name: "Background" },
-      { name: "SkinDevil" },
+      { name: "Skin" },
       { name: "EyeBrows" },
       { name: "Beard" },
       { name: "Mouth" },
