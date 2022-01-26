2c434c74568b47562dcbcdb90e7bd663d938fa65
PABourdais <pierre.alex.bourdais@gmail.com>
2022-01-26 23:58:07 +0100

    add more hand and shoes, modify cigar

--- src/config.js
+++ src/config.js
@@ -116,7 +116,7 @@ const layerConfigurations = [
     growEditionSizeTo: 500,
     layersOrder: [
       { name: "Background" },
-      { name: "SkinGhost" },
+      { name: "Skin" },
       { name: "EyeBrows" },
       { name: "Beard" },
       { name: "Mouth" },
