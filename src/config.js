e24f09a30455ba4328101c4212189bc09fc33c16
PABourdais <pierre.alex.bourdais@gmail.com>
2022-01-29 22:41:52 +0100

    modify some rarities

--- src/config.js
+++ src/config.js
@@ -116,7 +116,7 @@ const layerConfigurations = [
     growEditionSizeTo: 500,
     layersOrder: [
       { name: "Background" },
-      { name: "Skin" },
+      { name: "SkinDevil" },
       { name: "EyeBrows" },
       { name: "Beard" },
       { name: "Mouth" },
