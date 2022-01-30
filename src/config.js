c5f5ac6260b604ad39e9a212b4d43894d85fa871
PABourdais <pierre.alex.bourdais@gmail.com>
2022-01-30 15:12:02 +0100

    add more eyes

--- src/config.js
+++ src/config.js
@@ -116,7 +116,7 @@ const layerConfigurations = [
     growEditionSizeTo: 500,
     layersOrder: [
       { name: "Background" },
-      { name: "SkinDevil" },
+      { name: "Skin" },
       { name: "EyeBrows" },
       { name: "Beard" },
       { name: "Mouth" },
